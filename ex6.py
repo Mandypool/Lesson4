08685d03de8215bd36733d6feac61ed8e9227fa6
Chingis <gonnagowest@mai..ru>
2021-03-17 17:12:48 +0700

    Lesson4

--- ex6.py
+++ ex6.py
@@ -7,3 +7,25 @@
 # При достижении числа 10 — завершаем цикл. Вторым пунктом необходимо предусмотреть условие,
 # при котором повторение элементов списка прекратится.
 
+from itertools import count
+from itertools import cycle
+
+
+def my_count_func(start_number, stop_number):
+    for el in count(start_number):
+        if el > stop_number:
+            break
+        else:
+            print(el)
+
+
+def my_cycle_func(my_list, iteration):
+    i = 0
+    iter = cycle(my_list)
+    while i < iteration:
+        print(next(iter))
+        i += 1
+
+
+my_count_func(start_number=int(input("Введите стартовое число: ")), stop_number=int(input("Введите конечное число: ")))
+my_cycle_func(my_list=[3, 9], iteration=int(input("Введите повторение элементов списка: ")))
